3d5d546d56908095fd63ad6b2a9599cf33f22713
Ahmet Behdioğlu <ahmetuestcostaj@gmail.com>
2025-04-29 20:01:22 +0300

    hello world completed

---
+++ JavaBeginnerCourse/src/FirstClass.java
@@ -0,0 +1,7 @@
+public class FirstClass {
+    public static void sayHelloWorld(){
+        String message = "Hello World";
+        System.out.println(message);
+        // Statement is a complete command to be executed. It can include one or more expressions.
+    }
+}
